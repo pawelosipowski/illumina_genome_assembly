112b1134107f1328652403ce447691db1f0be51a
Pawel <pawel.sggw@gmail.com>
2016-01-18 20:37:45 +0100

    start

--- assembly.sh
+++ assembly.sh
@@ -1,8 +1,22 @@
+#assembly script
+#18.1.2016 Pawel Osipowski
+#folder should contain paired fastq files named like this:
+#<read_name>_fw.fq <read_name>_rv.fq
+
+#SYNOPSIS
+./assembly.sh read_name
+
 #read preparation:
 #trimmomatic
-java -jar ~/biosoft/read_processing/Trimmomatic-0.32/trimmomatic-0.32.jar PE -phred33 illumina_r1_musket_k17_trimmed_to_50x.fastq illumina_r2_musket_k17_trimmed_to_50x.fastq tt_r1_illumina_80x.fastq tt_r1_illumina_80x_up.fastq tt_r2_illumina_80x.fastq tt_r2_illumina_80x_up.fastq ILLUMINACLIP:TruSeq-PE.fa:2:30:15 TRAILING:30 MINLEN:50
+java -jar ./soft/Trimmomatic-0.32/trimmomatic-0.32.jar PE -phred33 "$1"_fw.fq "$1"_rv.fq tt_"$1"_fw.fq tt_"$1"_fw_unpaired.fq tt_"$1"_rv.fq tt_"$1"_rv_unpaired.fq ILLUMINACLIP:TruSeq-PE.fa:2:30:15 TRAILING:30 MINLEN:50
+
+#creating file with read file list for soapdenovo
+echo ./"$1"_fw.fq >> "$1".lst
+echo ./"$1"_fw_unpaired.fq >> "$1".lst
+echo ./"$1"_rv.fq >> "$1".lst
+echo ./"$1"_rv_unpaired.fq >> "$1".lst
 
 #soap kmer filtering
-KmerFreq_HA -k 23 -t 20 -i 100000000 -l ../read.lst -p b10_tt_23mer
+./soft/SOAPdenovo2-src-r240/ KmerFreq_HA -k 23 -t 20 -i 100000000 -l ../read.lst -p b10_tt_23mer
 Corrector_HA -k 23 -l 2 -e 1 -w 1 -q 30 -t 20 -Q 64 -o 3 b10_tt_23mer.freq.gz ../read.lst
 
